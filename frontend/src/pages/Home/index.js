66ef8bc76f73421d8ef45877f2b0e21199a6d3f3
Tamir Konor <Tamir.konor@gmail.com>
2020-08-16 16:06:21 +0300

    Adding TradingWidget on home page

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,15 +5,17 @@ import { Container, Row, Col, Nav, Navbar, Card, Form, Button, FormControl, Inpu
 import Header from '../Header';
 import css from '../style.css'
 import Dashboard from '../Dashboard';
-
+import {Helmet} from "react-helmet";
+import TradingViewWidget from 'react-tradingview-widget';
 let ind = 0;
 
 const Home = () => {
 
+    
     return (
         <>
             <div>
-                <Row style={{ backgroundColor: "#2196f3", width: "100%", marginLeft: "0px", marginRight: "0px" }}>
+                <Row style={{ backgroundColor: "#00007f", width: "100%", marginLeft: "0px", marginRight: "0px", paddingTop: 25}}>
                     <Col md={1}></Col>
                     <Col md={10}>
                         <div>
@@ -21,18 +23,36 @@ const Home = () => {
                         </div>
                         <div className="header-content">
                             <b>
-                                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.kjwqh dkjhk jsahd
-                                kjashdk jashd kjashd kjashd kjashd kjashd lasjdh asokdjhla sjdh alkjsdh laskdjhaslkjdh laksdjh alskjdh laksdhaljdshlkasjd lkajsdhl asjdhlkajshd
-                                lkajsdh kjlashd lkjashd lkjashdlkjashd lasjdh lkjashd lakjshd lkjasdh ljadh laksjdhipwuehef iuweh hakljh akdfjh kjsdfh lskdjhf lsdkjfh lsdfhjkldf lk
+                               AI-LGO Systems Ltd developed algo-trading robots based on advanced machine learning models that were successffuly deployed in medical data-mining and mortality prediction models.
+                               These models can provide accurate prediction on assets trends on daily trading. The algo-trading signals are sent to the trading strategy robots for long/short buy/sell instructions. 
+                               The model are optimized for low-risk trading policies.
                             </b>
                         </div>
                     </Col>
                     <Col md={1}></Col>
                 </Row>
-                <Row style={{ width: "100%", marginLeft: "0px", marginRight: "0px" }}>
+                <Row style={{ width: "100%", marginLeft: "0px", marginRight: "0px", backgroundColor: "#00007f" }}>
                     <Col md={1}></Col>
-                    <Col style={{ marginTop: 50 }} md={10}>
-                        <Card><Dashboard /></Card>
+                    <Col style={{ marginTop: 25 }} md={10}>
+                    <TradingViewWidget symbol="NASDAQ:AAPL" 
+                        interval = "1"
+                        hide_side_toolbar = "false"
+                        allow_symbol_change = "true"
+                        details = "true"
+                        theme = "lite"
+                        width = "100%"
+                        backgroundColor = "white"
+                        
+
+                    />
+                    <div class="tradingview-widget-copyright">
+         by Ai-Lgo</div>
+        
+
+                   
+
+
+                        {/* <Card><Dashboard /></Card> */}
                     </Col>
                     <Col md={1}></Col>
                 </Row>
